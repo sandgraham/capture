f99f6b1c93e85118c90ca7e4f11b43e5295ec1ff
sandgraham <grahammurdoch8@gmail.com>
2016-02-26 16:53:29 -0800

    initial commit of sorta working picture taking

---
+++ page.js
@@ -0,0 +1,80 @@
+var originalOverflowStyle, originalX, originalY;
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.action === 'createArrangements') createArrangements(sendResponse);
+    if (message.action === 'scrollPage') scrollPage(message.x, message.y, sendResponse);
+    if (message.action === 'cleanUp') cleanUp();
+});
+
+function scrollPage(x, y, sendResponse) {
+    window.scrollTo(x, y);
+    sendResponse();
+}
+
+function cleanUp() {
+    document.documentElement.style.overflow = originalOverflowStyle;
+    window.scrollTo(originalX, originalY);
+}
+
+function max(nums) {
+    return Math.max.apply(Math, nums.filter(function(x) { return x; }));
+}
+
+// return page data and arrangements to popup.js
+function createArrangements(sendResponse) {
+    var body = document.body,
+        widths = [
+            document.documentElement.clientWidth,
+            document.body.scrollWidth,
+            document.documentElement.scrollWidth,
+            document.body.offsetWidth,
+            document.documentElement.offsetWidth
+        ],
+        heights = [
+            document.documentElement.clientHeight,
+            document.body.scrollHeight,
+            document.documentElement.scrollHeight,
+            document.body.offsetHeight,
+            document.documentElement.offsetHeight
+        ],
+        fullWidth = max(widths),
+        fullHeight = max(heights),
+        windowWidth = window.innerWidth,
+        windowHeight = window.innerHeight,
+        arrangements = [],
+        // pad the vertical scrolling to try to deal with
+        // sticky headers, 250 is an arbitrary size
+        scrollPad = 200,
+        yDelta = windowHeight - (windowHeight > scrollPad ? scrollPad : 0),
+        xDelta = windowWidth,
+        yPos = fullHeight - windowHeight,
+        xPos;
+
+    // During zooming, there can be weird off-by-1 types of things...
+    if (fullWidth <= xDelta + 1) {
+        fullWidth = xDelta;
+    }
+
+    originalX = window.scrollX;
+    originalY = window.scrollY;
+    originalOverflowStyle = document.documentElement.style.overflow;
+    document.documentElement.style.overflow = 'hidden';
+
+    while (yPos > -yDelta) {
+        xPos = 0;
+        while (xPos < fullWidth) {
+            arrangements.push([xPos, yPos]);
+            xPos += xDelta;
+        }
+        yPos -= yDelta;
+    }
+
+    sendResponse({
+        arrangements: arrangements,
+        pageData: {
+            totalWidth: fullWidth,
+            totalHeight: fullHeight,
+            devicePixelRatio: window.devicePixelRatio
+        }
+    });
+}
---
+++ popup.js
@@ -0,0 +1,111 @@
+var screenshot,
+    contentUrl = '',
+    arrangements,
+    pageData,
+    tab;
+
+function downloadImage() {
+    var dataURI = screenshot.canvas.toDataURL();
+    var byteString = atob(dataURI.split(',')[1]);
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    var ab = new ArrayBuffer(byteString.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+    var blob = new Blob([ab], {type: mimeString});
+    var size = blob.size + (1024/2);
+    var name = contentUrl.split('?')[0].split('#')[0];
+    if (name) {
+        name = name
+            .replace(/^https?:\/\//, '')
+            .replace(/[^A-z0-9]+/g, '-')
+            .replace(/-+/g, '-')
+            .replace(/^[_\-]+/, '')
+            .replace(/[_\-]+$/, '');
+        name = '-' + name;
+    } else {
+        name = '';
+    }
+    name = 'screencapture' + name + '-' + Date.now() + '.png';
+
+    function onwriteend() {
+        chrome.downloads.download({url: 'filesystem:chrome-extension://' + chrome.i18n.getMessage('@@extension_id') + '/temporary/' + name})
+    }
+
+    function errorHandler() {
+        console.log('error creating image for download')
+    }
+
+    window.webkitRequestFileSystem(window.TEMPORARY, size, function(fs){
+        fs.root.getFile(name, {create: true}, function(fileEntry) {
+            fileEntry.createWriter(function(fileWriter) {
+                fileWriter.onwriteend = onwriteend;
+                fileWriter.write(blob);
+            }, errorHandler);
+        }, errorHandler);
+    }, errorHandler);
+}
+
+function processArrangements() {
+    if (!arrangements.length) {
+        downloadImage();
+        chrome.tabs.sendMessage(tab.id, {action: 'cleanUp'});
+        return;
+    }
+
+    var next = arrangements.shift();
+
+    chrome.tabs.sendMessage(tab.id, {action: 'scrollPage', x:next[0], y:next[1]}, null, function(response) {
+        captureArrangement(next[0], next[1]);
+    });
+}
+
+function captureArrangement(x, y) {
+    var scale = pageData.devicePixelRatio && pageData.devicePixelRatio !== 1 ? 1 / pageData.devicePixelRatio : 1;
+
+    if (scale !== 1) {
+        x = x / scale;
+        y = y / scale;
+        pageData.totalWidth = pageData.totalWidth / scale;
+        pageData.totalHeight = pageData.totalHeight / scale;
+    }
+
+    if (!screenshot.canvas) {
+        var canvas = document.createElement('canvas');
+        canvas.width = pageData.totalWidth;
+        canvas.height = pageData.totalHeight;
+        screenshot.canvas = canvas;
+        screenshot.ctx = canvas.getContext('2d');
+    }
+
+    chrome.tabs.captureVisibleTab(null, {format: 'png'}, function(dataUrl) {
+        if (dataUrl) {
+            var image = new Image();
+            image.onload = function() {
+                screenshot.ctx.drawImage(image, x, y);
+                processArrangements();
+            };
+            image.src = dataUrl;
+        }
+    });
+}
+
+document.getElementById('start').addEventListener('click', function() {
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        tab = tabs[0];
+
+        chrome.tabs.executeScript(null, {file: 'page.js'}, function(result) {
+            screenshot = {};
+            contentUrl = tab.url;
+
+            chrome.tabs.sendMessage(tab.id, {action: 'createArrangements'}, null, function(response) {
+                arrangements = response.arrangements;
+                pageData = response.pageData;
+
+                processArrangements();
+            });
+        });
+    });
+});
